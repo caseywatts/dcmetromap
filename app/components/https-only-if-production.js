cfff9df18f146d8011066af97e16459cf9e8910c
Casey Watts <casey.s.watts@gmail.com>
2017-11-19 17:51:16 -0500

    only do https redirect if it's production

--- app/components/https-only-if-production.js
+++ app/components/https-only-if-production.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
+import config from 'dcmetromap/config/environment';
 
 export default Ember.Component.extend({
   didInsertElement() {
     this._super(...arguments);
-    const isProduction = false;
+    const isProduction = (config.environment === 'production');
     if (isProduction) {
       if (window.location.protocol != 'https') {
         window.location.protocol = 'https';
